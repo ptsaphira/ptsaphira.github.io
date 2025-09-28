c08ee5981a519619142cc7dc7013871b6585b3d8
modeles <icreditares@gmail.com>
2025-09-28 12:38:28 +0100

    app

--- src/script/main.js
+++ src/script/main.js
@@ -15,6 +15,25 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+     $(document).ready(function () {
+    const popupKey = "promoPopupLastClosed";
+    const lastClosed = localStorage.getItem(popupKey);
+    const now = new Date().getTime();
+    const hours72 = 72 * 60 * 60 * 1000;
+
+    // Só abre se nunca fechou OU já passaram 72h
+    if (!lastClosed || now - lastClosed > hours72) {
+      setTimeout(() => {
+        $("#promoPopup").modal("show");
+      }, 2000); // espera 2s antes de abrir
+    }
+
+    // Ao fechar, salva o horário
+    $("#promoPopup").on("hidden.bs.modal", function () {
+      localStorage.setItem(popupKey, new Date().getTime());
+    });
+  });
+
     // Add any other global JavaScript functionalities here
 });
 
